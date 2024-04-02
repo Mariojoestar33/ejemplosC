bb4ddc3b22b072d1475d037920aac7b2bac6877d
Mariojoestar33 <mariano.pena15@gmail.com>
2024-04-02 11:14:08 -0600

    Agrega función para encontrar el máximo de un arreglo utilizando punteros

--- minMaxAPuntadores.c
+++ minMaxAPuntadores.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+int maximo(int* arr, int tam);
+
 int main() {
-    int arreglo []= {1, 2, 3, 434, 23238};
-    int* p = arreglo;
+    int arreglo [] = { 0, 1, 2, 3, 4, 5, 121892, 234234};
+    printf("El elemento maximo del arreglo es de: %d\n", maximo(arreglo, (int)(sizeof(arreglo)/sizeof(arreglo[0]))));
+
     return 0;
+}
+
+int maximo(int* arr, int tam) {
+    int maximmo = *arr;
+    for(int i = 0; i < tam; i++) {
+        if(*(arr + i) > maximmo) {
+            maximmo = *(arr + i);
+        }
+    }
+    return maximmo;
 }
