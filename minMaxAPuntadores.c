0c1630e1f7d358b3d4384a71e56b12118b0d90bd
Mariojoestar33 <mariano.pena15@gmail.com>
2024-04-02 11:16:38 -0600

    Agregado función para encontrar el elemento mínimo en el arreglo

--- minMaxAPuntadores.c
+++ minMaxAPuntadores.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int maximo(int* arr, int tam);
+int minimo(int* arr, int tam);
 
 int main() {
-    int arreglo [] = { 0, 1, 2, 3, 4, 5, 121892, 234234};
+    int arreglo [] = { 0, 1, 2, 3, 4, 5, 121892, 234234, -100};
     printf("El elemento maximo del arreglo es de: %d\n", maximo(arreglo, (int)(sizeof(arreglo)/sizeof(arreglo[0]))));
-
+    printf("El elemento maximo del arreglo es de: %d\n", minimo(arreglo, (int)(sizeof(arreglo)/sizeof(arreglo[0]))));
     return 0;
 }
 
@@ -17,4 +18,14 @@ int maximo(int* arr, int tam) {
         }
     }
     return maximmo;
+}
+
+int minimo(int* arr, int tam) {
+    int min = *arr;
+    for(int i = 0; i < tam; i++) {
+        if(*(arr + i) < min) {
+            min = *(arr + i);
+        }
+    }
+    return min;
 }
